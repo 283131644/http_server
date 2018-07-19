cd0caa1639edd1788d34426dc994ff8634c641f2
lishuai <lishuai@bytedance.com>
2018-07-19 20:59:20 +0800

    更新注释
    Change-Id: Iba10f801ffd6f5adc80a0c0c334bc35241505f51

--- demo/server.c
+++ demo/server.c
@@ -23,8 +23,8 @@ int main(int argc, char *argv[]) {
     bzero(&addr, sizeof(addr));
     addr.sin_family = AF_INET;
 
-    // What does the `htonl` and `htons` mean?
-    // What is `INADDR_ANY`?
+    // 理解一下 htonl 和 htons 是干什么的
+    // INADDR_ANY 表示什么？
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(port);
 
@@ -47,10 +47,10 @@ int main(int argc, char *argv[]) {
     int clientfd = -1;
     int ret = 0;
     while ((clientfd = accept(listenfd, NULL, NULL)) >= 0) {
-        // How to print the client's ip and port?
+        // 如何获取客户端 IP:PORT
         printf("Client connected: %d\n", clientfd);
 
-        // Understand `fork`
+        // 理解一下fork
         ret = fork();
         if (ret < 0) {
             perror("fork");
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
         }
         if (ret == 0) {
             handle_client(root_path, clientfd);
-            // If this `exit` miss, what will happend?
+            // 如果缺少这个exit，会不会有问题？
             exit(0);
         }
     }
@@ -72,7 +72,7 @@ void handle_client(char *root_path, int fd) {
     req[req_len] = '\0';
 
     int n = 0;
-    // Why we need a loop here?
+    // 为什么需要一个循环来读取
     while (strstr(req, "\r\n\r\n") == NULL) {
         n = read(fd, req + req_len, MAX_REQ - req_len);
         if (n < 0) {
@@ -87,14 +87,16 @@ void handle_client(char *root_path, int fd) {
         req[req_len] = '\0';
     }
 
-    // Get the URI
+    // 获取 URI
     strtok(req, " ");
     char *uri = strtok(NULL, " ");
 
     printf("[%d] request_uri=%s\n", fd, uri);
 
-    // Replace it to read the file in the dir root_path base on the uri
-    // Response "Hello World";
+    // 这里只是给个Demo，直接响应"Hello World";
+    // 按要求替换成相应的文件内容，文件路径为<root_path>/<uri>
+    // 考察一下怎么防攻击：比如读到其它目录下的文件
+    // 如果文件没有找到，应该怎么办？
     char *str = "HTTP/1.1 200 OK\r\n"
         "Content-Length: 12\r\n"
         "Content-Type: text/plain\r\n"
